32cb337cc6ece2725c4137d2ed639768700e7303
jairo cogollo <mickt681@gmail.com>
2022-06-11 04:04:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
       viewCompleted: false,
       activeItem: {
         nombre_empresa: "",
+        completed: "",
         direccion: "",
         nit: "",
         telefono: ""
@@ -21,7 +22,7 @@ class App extends Component {
   }
   refreshList = () => {
     axios
-      .get("http://localhost:8000/api/todos/")
+      .get("http://localhost:8000/api/datos-empresas/")
       .then(res => this.setState({ empresas: res.data }))
       .catch(err => console.log(err));
   };
@@ -72,17 +73,17 @@ class App extends Component {
     this.toggle();
     if (item.id) {
       axios
-        .put(`http://localhost:8000/api/todos/${item.id}/`, item)
+        .put(`http://localhost:8000/api/datos-empresas//${item.id}/`, item)
         .then(res => this.refreshList());
       return;
     }
     axios
-      .post("http://localhost:8000/api/todos/", item)
+      .post("http://localhost:8000/api/datos-empresas/", item)
       .then(res => this.refreshList());
   };
   handleDelete = item => {
     axios
-      .delete(`http://localhost:8000/api/todos/${item.id}`)
+      .delete(`http://localhost:8000/api/datos-empresas/${item.id}`)
       .then(res => this.refreshList());
   };
   createItem = () => {
